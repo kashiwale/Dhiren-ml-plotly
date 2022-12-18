2aa537109b3d3ebae9daea1ce574addcac29b02f
kashiwale <kashiwale@yahoo.com>
2022-12-18 06:33:21 -0500

    Add tabs to app.py

--- app.py
+++ app.py
@@ -1,30 +1,63 @@
-import dash
+import dash as ds
 #import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash import html
 
 #Themes : "https://dash-bootstrap-components.opensource.faculty.ai/docs/themes/#available-themes"
 #app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.YETI])
+app = ds.Dash(__name__, external_stylesheets=[dbc.themes.YETI])
 
-app.layout = html.Div([
-    html.H1('Poverty And Equity Database',
-            style={'color': 'blue',
-            'fontSize': '40px'}),
-            html.H2('The World Bank'),
-            html.P('Key Facts:'),
-            #html.Ul([
-            html.Ol([
-                html.Li('Number of Economies: 170'),
-                html.Li('Temporal Coverage: 1974 - 2019'),
-                html.Li('Update Frequency: Quarterly'),
-                html.Li('Last Updated: March 18, 2020'),
-                html.Li([
-                    'Source: ',
-                html.A('https://datacatalog.worldbank.org/dataset/poverty-and-equity-database', 
-                href='https://datacatalog.worldbank.org/dataset/poverty-and-equity-database')
-                        ])
-                ])
+app.layout = dbc.Tabs([
+    dbc.Tab([
+        html.Div([
+                html.H1('OM Shri Ganeshay Namah',
+                style={'color': 'blue',
+                        'fontSize': '40px'}
+                
+                )
+        ])
+
+
+    ],label='Dhiren'),
+
+
+
+    dbc.Tab([
+        dbc.Tabs([
+        dbc.Tab([
+        html.Div([
+        html.H1('Poverty And Equity Database',
+                style={'color': 'blue',
+                'fontSize': '40px'}),
+                html.H2('The World Bank'),
+                html.P('Key Facts:'),
+                #html.Ul([
+                html.Ol([
+                    html.Li('Number of Economies: 170'),
+                    html.Li('Temporal Coverage: 1974 - 2019'),
+                    html.Li('Update Frequency: Quarterly'),
+                    html.Li('Last Updated: March 18, 2020'),
+                    html.Li([
+                        'Source: ',
+                    html.A('https://datacatalog.worldbank.org/dataset/poverty-and-equity-database', 
+                    href='https://datacatalog.worldbank.org/dataset/poverty-and-equity-database')
+                            ])
+                    ])
+                    ])
+        ]   , label='Key Facts'),
+
+        dbc.Tab([
+            html.Ul([
+                html.Br(),
+                html.Li('Book title: Interactive Dashboards and Data Apps with Plotly and Dash'),
+                html.Li(['GitHub repo: ',
+                        html.A('https://github.com/PacktPublishing/Interactive-Dashboards-and-Data-Apps-with-Plotly-and-Dash',
+                                href='https://github.com/PacktPublishing/Interactive-Dashboards-and-Data-Apps-with-Plotly-and-Dash')])
+            ])
+        ], label='Project Info')
+        ])
+
+                ] ,label='Plotly')
                 ])
 
 
