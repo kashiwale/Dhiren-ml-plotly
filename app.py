db0117e3d88decf37a8121386d98c8dc0a23fe21
kashiwale <kashiwale@yahoo.com>
2022-12-18 13:42:26 -0500

    Add Interactive callbacks

--- app.py
+++ app.py
@@ -2,12 +2,37 @@ import dash as ds
 #import dash_html_components as html
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.express as px
 from dash import html
+
+happiness = pd.read_csv("world_happiness.csv")
+region_options=[{'label':i, 'value':i} for i in happiness['region'].unique()]
+country_options=[{'label':i, 'value':i} for i in happiness['country'].unique()]
+line_fig=px.line(happiness[happiness['country']=='United States'], x='year',y='happiness_score', title='Happiness Score in the USA')
+
 
 #Themes : "https://dash-bootstrap-components.opensource.faculty.ai/docs/themes/#available-themes"
 #app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+
+
+
+
+
 app = ds.Dash(__name__, external_stylesheets=[dbc.themes.YETI])
 server = app.server
+
+
+@app.callback(
+    Output(component_id='output-text',component_property='children'), 
+    Input(component_id='input-text',component_property='value')
+)
+def update_output_div(input_text):
+    return f'Text: {input_text}'
+
+    
 app.layout = dbc.Tabs([
     dbc.Tab([
         html.Div([
@@ -15,7 +40,9 @@ app.layout = dbc.Tabs([
                 style={'color': 'blue',
                         'fontSize': '40px'}
                 
-                )
+                ),
+                dcc.Input(id='input-text',value='Change this text',type='text' ),
+                html.Div(id='output-text')
         ])
 
 
@@ -69,7 +96,14 @@ app.layout = dbc.Tabs([
                     html.A('World Happiness Report Data Source', 
                     href='https://worldhappiness.report/',
                     target = '_blank')
-                ])
+                ]),
+                dcc.RadioItems(options=region_options,
+                value='North America'),       
+                dcc.Checklist(options=region_options,
+                value=['North America']) ,           
+                dcc.Dropdown(options=country_options,
+                value=['United States'])  ,
+                dcc.Graph(figure=line_fig)         
             ])
         ], label='World Happiness')
         ])
